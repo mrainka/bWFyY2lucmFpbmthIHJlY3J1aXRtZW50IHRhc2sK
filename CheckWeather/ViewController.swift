aa2aab4e4a85b0f677c1ade757ebc6a8a097606e
Marcin Rainka <marcin.rainka@gmail.com>
2020-05-28 20:20:09 +0200

    Add empty project

---
+++ CheckWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckWeather
+//
+//  Created by Marcin Rainka on 28/05/2020.
+//  Copyright © 2020 Marcin Rainka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
